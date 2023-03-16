5a7aa907a9e2f7c634f63cd79abd169c50929239
Mateus <mateuscolvr@gmail.com>
2023-03-15 22:26:55 -0300

    Remove grupos

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,12 +6,4 @@ require('mat.lsp')
 require('mat.utils')
 require('aniseed.env').init()
 
-local function autocmd(event, opts)
-	vim.api.nvim_create_autocmd(event, opts)
-end
-
 vim.cmd.colorscheme('gruvbox')
-
-autocmd('BufWritePost', {pattern = 'keymapping.lua', command = 'source <afile>', group = vim.api.nvim_create_augroup('keymappings', {clear = true})})
--- autocmd('BufWritePre', {pattern = '*.{tsx,jsx}', command = 'Neoformat prettier', group = vim.api.nvim_create_augroup('prettier', {clear = true})})
-autocmd('BufWritePost', {pattern = '*.norg', command = 'Neorg tangle current-file', group = vim.api.nvim_create_augroup('tangle', {clear=true})})
